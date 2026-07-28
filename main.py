b5fa8627e19d3d6a05ce95fdf13b93f547541d8a
Fernando Morales Flores <fmoralesflores0@gmail.com>
2026-07-28 19:27:42 -0400

    feat: implementar autenticacion del administrador

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
+from app.api import admin
 
 app = FastAPI(
     title="ChickenBot Delivery API",
-    description="Backend para la gestion de pedidos de ChickenBot Delivery",
+    description="Backend para la gestión de pedidos",
     version="1.0.0"
 )
 
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="super-secret-key"
+)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
+app.include_router(admin.router)
 
 @app.get("/")
 async def root():

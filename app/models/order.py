4c63ae94db7e8a706858571c0c514b77cb56247e
Fernando Morales Flores <fmoralesflores0@gmail.com>
2026-07-28 17:22:29 -0400

    feat: crear modelo Order

---
+++ app/models/order.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.base import Base
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    status = Column(String(30), nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="orders")

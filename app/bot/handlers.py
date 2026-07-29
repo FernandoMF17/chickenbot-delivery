bd4552de43b5bacc49fd26e5b331c934784d9360
Fernando Morales Flores <fmoralesflores0@gmail.com>
2026-07-28 23:57:18 -0400

    feat: mostrar categorias disponibles en el bot de telegram

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -2,6 +2,11 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.ext import MessageHandler, filters
 
+from telegram import ReplyKeyboardMarkup
+
+from app.db.session import SessionLocal
+from app.models.category import Category
+
 from app.bot.keyboards import main_keyboard
 
 
@@ -29,6 +34,34 @@ Selecciona una opción del menú 👇
         reply_markup=main_keyboard()
     )
 
+def categories_keyboard():
+
+    db = SessionLocal()
+
+    try:
+
+        categories = (
+            db.query(Category)
+            .order_by(Category.name)
+            .all()
+        )
+
+    finally:
+        db.close()
+
+    keyboard = []
+
+    for category in categories:
+        keyboard.append([category.name])
+
+    keyboard.append(["⬅️ Menú principal"])
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
+
+
 async def menu(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
@@ -39,7 +72,15 @@ async def menu(
     if text == "🍗 Ver categorías":
 
         await update.message.reply_text(
-            "Mostrando categorías..."
+            "Selecciona una categoría:",
+            reply_markup=categories_keyboard()
+        )
+
+    elif text == "⬅️ Menú principal":
+
+        await update.message.reply_text(
+            "Menú principal:",
+            reply_markup=main_keyboard()
         )
 
     elif text == "🛒 Mi carrito":
@@ -52,6 +93,15 @@ async def menu(
 
         await update.message.reply_text(
             "Todavía no tienes pedidos."
+        )
+    elif text in [
+        "Combos",
+        "Bebidas",
+        "Postres"
+    ]:
+
+        await update.message.reply_text(
+            f"Mostrando productos de {text}..."
         )
 
     else:
